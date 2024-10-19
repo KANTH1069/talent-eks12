0bea721da36c0de47dd7a8312e03201ae76a1891
EC2 Default User <ec2-user@ip-172-31-44-30.us-west-2.compute.internal>
2024-10-19 02:05:38 +0000

    update CIRD for subnets

--- main.tf
+++ main.tf
@@ -29,11 +29,11 @@ module "vpc" {
 
   name = "talent-vpc"
 
-  cidr = "10.0.0.0/23"
+  cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  private_subnets = ["10.0.1.0/26", "10.0.2.0/26", "10.0.3.0/26"]
-  public_subnets  = ["10.0.4.0/26", "10.0.5.0/26", "10.0.6.0/26"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
