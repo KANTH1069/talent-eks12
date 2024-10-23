3d21b944e533e7b4f921ab16a7f58a9dd1fbf07e
EC2 Default User <ec2-user@ip-172-31-44-30.us-west-2.compute.internal>
2024-10-23 01:48:10 +0000

    added locking with dynamodb table

--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "talent-eks-terraform-state-file"
     key    = "terraform.tfstate"
+    dynamodb_table = "talent-eks-tf-lockfile"
     region = "us-west-2"
   }
 }
