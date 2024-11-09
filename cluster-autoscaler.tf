6180f50ad4ba41eeec868112529b99d3c35ec621
EC2 Default User <ec2-user@ip-172-31-44-30.us-west-2.compute.internal>
2024-11-09 02:01:45 +0000

    fixed conflicts

--- cluster-autoscaler.tf
+++ cluster-autoscaler.tf
@@ -4,5 +4,6 @@ module "eks-cluster-autoscaler" {
   # insert the 3 required variables here
   cluster_name                     = module.eks.cluster_name
   cluster_identity_oidc_issuer     = module.eks.oidc_provider
+
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
 }
