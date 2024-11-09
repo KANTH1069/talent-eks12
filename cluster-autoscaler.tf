7b87c2368d360aaadbdc2a90fe672e23498c4a31
malleshdevops <81004732+malleshdevops@users.noreply.github.com>
2024-11-09 07:20:15 +0530

    Update cluster-autoscaler.tf

--- cluster-autoscaler.tf
+++ cluster-autoscaler.tf
@@ -1,11 +1,8 @@
-module "cluster_autoscaler" {
-  source = "git::https://github.com/DNXLabs/terraform-aws-eks-cluster-autoscaler.git"
-
-  enabled = true
-
-  cluster_name                     = module.eks.cluster_id
-  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
+module "eks-cluster-autoscaler" {
+  source  = "lablabs/eks-cluster-autoscaler/aws"
+  version = "2.2.0"
+  # insert the 3 required variables here
+  cluster_name                     = module.eks.cluster_name
+  cluster_identity_oidc_issuer     = module.eks.oidc_provider
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
-  aws_region                       = var.region
-  
 }
